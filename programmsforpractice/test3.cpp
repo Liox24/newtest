4295e4e450608d9cdf7c3b793e7a280860f8368c
Liox24 <149859919+Liox24@users.noreply.github.com>
2024-01-26 00:15:49 +0530

    Add files via upload

---
+++ programmsforpractice/test3.cpp
@@ -0,0 +1,15 @@
+//create a code for table 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter Number :";
+//     cin>>n;
+//     for (int i = 0 ; i < 10 ; i++){
+//         cout<<n<<" "<<i+1<<" "<<n*(i+1)<<endl;
+//     }
+// }
+
+// create a code for stack all of the stack 
+
